7ff7c20ff906fde8aa887417e10083be46521e15
sudharaD <sudharad7@gmail.com>
2022-10-14 12:16:43 +0530

    Input function

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ print("Hello World")
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
+
+print("Hello World!\nHello World!\nHello World!")
+print("Hello " + "Angela")
