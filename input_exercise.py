b1f0c542575bca8f8cc5a944605cfa2a9d1182c8
sudharaD <sudharad7@gmail.com>
2022-10-15 23:22:29 +0530

    input_challenge

---
+++ input_exercise.py
@@ -0,0 +1,11 @@
+name = input("What is your name? : ")
+
+count = len(name)
+displayText = "Your namme includes " + str(len(name)) + " character"
+
+if count > 1 or count < 1:
+    print(displayText + "s")
+else:
+    print(displayText)
+
+# print("Your namme includes " + str(len(name)) + " characters")
